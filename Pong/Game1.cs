7fdab456c550429393be09269fa20bebce94dbe6
JesseTabak <jesse.tabak1@gmail.com>
2023-09-18 00:44:22 +0200

    blue

--- Pong/Game1.cs
+++ Pong/Game1.cs
@@ -11,9 +11,21 @@ namespace Pong
         private GraphicsDeviceManager _graphics;
         SpriteBatch _spriteBatch;
         Rectangle screenRectangle;
+        Rectangle blueRectangle;
         Texture2D bal;
+        Vector2 bluePos;
+        Vector2 blueVelocity;
+        Texture2D blauweSpeler;
+        public string keyW = "keyW/Upblue";
+        public string keyS = "keyS/Downblue";
+        public float timepassed;
+
+       
+        public float movementSpeed;
+
         Rectangle balRectangle;
         double speed, angle;
+       
 
         public Game1()
         {
@@ -25,10 +37,12 @@ namespace Pong
         protected override void Initialize()
 
         {
+
+           
             screenRectangle = new Rectangle(0, 0, 1400, 800);
 
-            balRectangle.Width = 60;
-            balRectangle.Height = 60;
+            balRectangle.Width = 20;
+            balRectangle.Height = 20;
             balRectangle.X = screenRectangle.Width / 2 - balRectangle.Width / 2;
             balRectangle.Y = screenRectangle.Height / 2 - balRectangle.Height / 2;
             speed = 5;
@@ -38,6 +52,16 @@ namespace Pong
             _graphics.PreferredBackBufferHeight = screenRectangle.Height;
             _graphics.ApplyChanges();
 
+            blueRectangle.Width = 17;
+            blueRectangle.Height = 133;
+            blueRectangle.X = 10;
+            blueRectangle.Y = (int)bluePos.Y;
+            movementSpeed = 8;
+
+
+
+
+
             base.Initialize();
         }
 
@@ -45,6 +69,11 @@ namespace Pong
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             bal = Content.Load<Texture2D>("bal");
+            blauweSpeler = Content.Load<Texture2D>("blauweSpeler");
+
+            
+
+
         }
 
         // converts degrees to radians
@@ -59,6 +88,14 @@ namespace Pong
                 Exit();
 
            
+
+           
+
+            
+
+
+           
+
             int xSpeed = (int)(speed * Math.Cos(Radians(angle)));
             int ySpeed = (int)(speed * Math.Sin(Radians(angle)));
             balRectangle.X += xSpeed;
@@ -80,6 +117,53 @@ namespace Pong
             {
                 angle = 180 - angle;
             }
+
+
+           
+
+            KeyboardState keyState = Keyboard.GetState();
+
+
+
+
+            if (blueRectangle.Top >= screenRectangle.Top)
+            {
+                if (keyState.IsKeyDown(Keys.W)) { blueVelocity.Y = 1; }
+
+                if (keyState.IsKeyDown(Keys.S)) { blueVelocity.Y = -1; }
+
+                if (blueVelocity != Vector2.Zero) { blueVelocity.Normalize(); }
+            }
+            if (blueRectangle.Top < screenRectangle.Top)
+            { 
+                if (keyState.IsKeyDown(Keys.S)) { blueVelocity.Y = -1; } 
+            }
+
+
+
+            if (blueRectangle.Bottom >= screenRectangle.Bottom)
+            {
+                if (keyState.IsKeyDown(Keys.W)) { blueVelocity.Y = 1; }
+
+                if (keyState.IsKeyDown(Keys.S)) { blueVelocity.Y = -1; }
+
+                if (blueVelocity != Vector2.Zero) { blueVelocity.Normalize(); }
+            }
+            if (blueRectangle.Bottom < screenRectangle.Bottom)
+            {
+                if (keyState.IsKeyDown(Keys.W)) { blueVelocity.Y = 1; }
+            }
+
+
+
+            bluePos.Y -= blueVelocity.Y * movementSpeed;
+
+            blueVelocity = Vector2.Zero;
+
+            blueRectangle.Y = (int)bluePos.Y;
+
+           
+
             base.Update(gameTime);
         }
 
@@ -91,9 +175,12 @@ namespace Pong
             GraphicsDevice.Clear(Color.White); 
             
             _spriteBatch.Begin();
-            _spriteBatch.Draw(bal, balRectangle, Color.White);
-            _spriteBatch.End();
 
+            _spriteBatch.Draw(bal, balRectangle, Color.White);
+            _spriteBatch.Draw(blauweSpeler, blueRectangle , Color.White);
+
+            _spriteBatch.End();
+            
             base.Draw(gameTime);
         }
     }
